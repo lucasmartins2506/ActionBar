80172956a8f36618a8d491a0c225cad7a74fe292
lucasmartins2506 <lucasnalua25@gmail.com>
2019-09-30 22:38:00 -0300

    Menu-botoes

---
+++ app/src/main/java/br/com/etechoracio/actionbar/ActionBarActivity.java
@@ -0,0 +1,58 @@
+package br.com.etechoracio.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ActionBarActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_wifi:
+                Toast.makeText(this,"Wi-fi acionado !",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_sair:
+                finish();
+                return  true;
+
+            case R.id.action_bluetooth:
+                Toast.makeText(this,"Bluetooth acionado !",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_sobre:
+                Toast.makeText(this,". . . ",Toast.LENGTH_LONG).show();
+                return true;
+
+
+
+
+            default:
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_bar);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+
+    }
+}
